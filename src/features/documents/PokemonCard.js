10758f8161b6a54c053457b5c125fcf789149e89
Simon Billet <simon.billet@etudiant.univ-lr.fr>
2021-02-25 15:36:51 +0100

    fix variable non utilisées

--- src/features/documents/PokemonCard.js
+++ src/features/documents/PokemonCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {ajoutPokemonMesFavoris, requeteDetailPokemon, selectPokemonsListe} from "./pokemonsSlice";
+import {requeteDetailPokemon, selectPokemonsListe} from "./pokemonsSlice";
 
 
 export const PokemonCard = () => {
